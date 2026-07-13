cd2f98c1be0a6cb26e86221f30ca9aa7f5734ddc
Riteshpati001 <pramilapati008@gmail.com>
2026-07-13 12:37:22 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please add a name'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email address'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please enter a valid email address',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt timestamps
+  }
+);
+
+module.exports = mongoose.model('User', UserSchema);
